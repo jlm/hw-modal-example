11fb2ccbc118c7ab3eb3c35b92b6c79990a214ba
jlm <j.l.messenger@ieee.org>
2025-11-03 17:42:53 +0000

    Add a model, views and a route to it.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,6 +3,7 @@
 # your test database is "scratch space" for the test suite and is wiped
 # and recreated between test runs. Don't rely on the data there!
 
+# noinspection GrazieInspection
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
