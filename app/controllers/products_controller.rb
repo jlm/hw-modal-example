216caa17bc690e9842f0ebcabf5e72dc969f2d2d
jlm <j.l.messenger@ieee.org>
2025-11-03 22:21:10 +0000

    Get the modals working, with appropriate styling.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [ :show, :edit, :update ]
+  before_action :set_product, except: %i[index new create]
   def index
     @products = Product.all
   end
@@ -9,26 +9,33 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    render layout: "modal"
   end
 
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to @product
+      redirect_to products_path, notice: "Product successfully added!"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def edit
+    render layout: "modal"
   end
 
   def update
     if @product.update(product_params)
-      redirect_to @product
+      redirect_to products_path, notice: "Product successfully updated!"
     else
-      render :edit, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity, layout: "modal"
     end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path, notice: "Product was successfully destroyed."
   end
 
   private
